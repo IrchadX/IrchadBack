71711d99184332e2004bbb7ae790bb8b1c4ea732
Rania Gueddouche <gueddoucherania@gmail.com>
2025-06-11 21:17:57 +0100

    fixing conflicts

--- src/zones/zones.service.ts
+++ src/zones/zones.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+/*import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateZoneDto } from './dto/create-zone.dto';
 import { UpdateZoneDto } from './dto/update-zone.dto';
@@ -63,4 +63,4 @@ export class ZonesService {
     });
     return { message: `Zone ${id} deleted` };
   }
-}
+}*/
