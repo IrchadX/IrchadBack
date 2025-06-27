789821a43f268afc09c720fd679a2fccc03c768b
wissalcodes <lw_messikh@esi.dz>
2025-06-27 11:46:50 +0100

    typo

--- src/api/zone/zone.module.ts
+++ src/api/zone/zone.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ZoneService } from './zone.service';
 import { ZoneController } from './zone.controller';
-import { PrismaModule } from 'src/prisma/prisma.module'; // <-- ici
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule], // <-- ici
+  imports: [PrismaModule],
   controllers: [ZoneController],
   providers: [ZoneService],
 })
